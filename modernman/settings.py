08480b13b7d21c70e8fe6c8f768ab48a1887aaea
Chege CJN <chegecjn@gmail.com>
2024-06-09 16:50:45 +0300

    updated .gitignore: to exclude secret keys

--- modernman/settings.py
+++ modernman/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'djangodblog.DBLogMiddleware', 
 ]
 
 ROOT_URLCONF = 'modernman.urls'
