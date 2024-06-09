bf841a2f9278a263c0d9e87a16df084188bef635
Chege CJN <chegecjn@gmail.com>
2024-06-09 23:23:48 +0300

    set up templates and staticfiles

--- modernman/urls.py
+++ modernman/urls.py
@@ -15,7 +15,35 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from django.views.generic import TemplateView
+
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    path(
+        'admin/', 
+        admin.site.urls
+        ),
+
+    path(
+        '', 
+        TemplateView.as_view(template_name='index.html'),
+        name='home'
+        ),
+
+
+]   + static(
+    settings.STATIC_URL, 
+    document_root=settings.STATIC_ROOT
+    )
+
+
+
+    
+     
+
+
+       
+       
