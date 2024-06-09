bf030661593efac30fa92b068b53cf7241132d90
Chege CJN <chegecjn@gmail.com>
2024-06-09 16:43:15 +0300

    updated settings: added postgres database and db logging

--- modernman/settings.py
+++ modernman/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'djangodblog',
 ]
 
 MIDDLEWARE = [
@@ -70,16 +71,17 @@ TEMPLATES = [
 WSGI_APPLICATION = 'modernman.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
+# Database: postgres
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "modernman2.0",
+        "USER": "postgres",
+        "PASSWORD": "root",
+        "HOST": "localhost",
+        "client_encoding": "UTF8"
+        }
     }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
