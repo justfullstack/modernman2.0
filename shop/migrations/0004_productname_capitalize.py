8125bd8e82d5c839398518de5b901fe54041a40d
Chege CJN <chegecjn@gmail.com>
2024-06-14 16:05:39 +0300

    added a data migration to capitalize all product names

---
+++ shop/migrations/0004_productname_capitalize.py
@@ -0,0 +1,20 @@
+from django.db import migrations
+
+def capitalize(apps, schema_editor):
+    Product = apps.get_model('main', 'Product')
+    for product in Product.objects.all():
+        product.name = product.name.capitalize()
+        product.save()
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('shop', '0003_remove_productimage_image_and_more'),
+    ]
+
+    operations = [
+        migrations.RunPython(
+                    capitalize,
+                    migrations.RunPython.noop
+                    ),
+    ]
