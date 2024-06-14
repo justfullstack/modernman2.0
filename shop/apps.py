c4589a93af08aa31ac3a89b337c787c8dd9c03d7
Chege CJN <chegecjn@gmail.com>
2024-06-14 12:06:06 +0300

    updated shop app: added a ready() app to shop apps to automaticall load thumbnails on upload

--- shop/apps.py
+++ shop/apps.py
@@ -4,3 +4,13 @@ from django.apps import AppConfig
 class ShopConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'shop'
+
+class MainConfig(AppConfig):
+    """make sure this file is initialized when the Django application is launched by the internal Django application registry."""
+    name = 'main'
+
+    def ready(self):
+        from . import signals
+
+
+
