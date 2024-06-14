72e7bf6d446cf4c872d43d004968799b97dfb3b3
Chege CJN <chegecjn@gmail.com>
2024-06-14 13:04:41 +0300

    updated shop app: added tests for ActiveManager

---
+++ shop/tests/test_models.py
@@ -0,0 +1,25 @@
+from decimal import Decimal
+from django.test import TestCase
+import models
+
+
+class TestModel(TestCase):
+    def testProductActiveManager(self):
+        models.Product.objects.create(
+                name="First Product Testing Active Manager",
+                price=Decimal("10.00")
+                )
+        
+        models.Product.objects.create(
+                name="Second Product Testing Active Manager",
+                price=Decimal("2.00")
+                )
+        
+        # not active
+        models.Product.objects.create(
+                name="Third Product Testing Active Manager",
+                price=Decimal("2.00"),
+                active=False)
+        
+        # only two products should be active
+        self.assertEqual(len(models.Product.objects.active()), 2)
